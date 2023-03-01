f5f8579622ca4035afc4f6fb1ee6d27a58c95ff4
Joerg Cluesserath <joerg.cluesserath@cosee.biz>
2023-03-01 13:32:24 +0100

    prepare for webcop

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/index.scss'
-import {FaqPage} from "@/views/Faq/FaqPage";
-import {StopwatchPage} from "@/views/Stopwatch/StopwatchPage";
+/*import {FaqPage} from "@/views/Faq/FaqPage";
+import {StopwatchPage} from "@/views/Stopwatch/StopwatchPage";*/
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FaqPage />
-    <StopwatchPage />
+{/*    <FaqPage />
+    <StopwatchPage />*/}
   </React.StrictMode>,
 )
